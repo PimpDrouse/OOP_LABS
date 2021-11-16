d4c466b6a6bce407c8d38a937e73231d05eaa8b2
PimpDrouse <Volegov2002@ya.ru>
2021-11-16 21:53:09 +0300

    dsa

---
+++ LABA_4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using LABA_4.ClassesAndInterfaces;
+
+namespace LABA_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentCollection studentCollection1 = new StudentCollection(StudentsCount, "WOW");
+            StudentCollection studentCollection2 = new StudentCollection(StudentsCount, "HEHE");
+
+            Journal journal1 = new Journal();
+            Journal journal2 = new Journal();
+
+            studentCollection1.StudentCountChanged += journal1.Event;
+            studentCollection1.StudentsReferenceChanged += journal1.Event;
+            studentCollection1.StudentsReferenceChanged += journal2.Event;
+            studentCollection2.StudentsReferenceChanged += journal2.Event;
+            
+            studentCollection1.AddDefaults();
+            studentCollection1.Remove(0);
+            studentCollection1.Remove(0);
+            studentCollection1.AddStudents(new Student());
+            studentCollection1[0] = new Student();
+            
+            studentCollection2.AddDefaults();
+            studentCollection2.Remove(0);
+            studentCollection2.Remove(0);
+            studentCollection2.AddStudents(new Student());
+            studentCollection2[0] = new Student();
+            
+            Console.WriteLine(journal1.ToString() + "\n");
+            Console.WriteLine(journal2.ToString());
+        }
+        
+        
+        private static void StudentsCount(object source, StudentListHandlerEventArgs args)
+        {
+            //Console.WriteLine(args.ToString());
+        }
+    }
+}
